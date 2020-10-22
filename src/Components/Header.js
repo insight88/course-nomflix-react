10fa65f48503227b66396beb7466c1e187460f5f
insight88 <kpkim1988@gmail.com>
2020-10-22 18:43:46 +0900

    0.2 styled component & header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,61 @@
 import React from "react"
+import { Link, withRouter } from "react-router-dom";
+// * withRouter : 다른 component를 wrapping하는 component (고차원 component)
+// * withRouter는 내부 component에 props를 전달
+import styled from "styled-components"
 
-export default () => (
-  <header>
-    <ul>
-      <li>
-        <a href="/">Movies</a>
-      </li>
-      <li>
-        <a href="/tv">TV</a>
-      </li>
-      <li>
-        <a href="/search">Search</a>
-      </li>
-    </ul>
-  </header>
-)
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+
+const List = styled.ul`
+  display: flex;
+`
+
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+  border-bottom: 3px solid
+    ${props => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom 0.5s ease-in-out;
+`;
+
+
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+// * react-router-dom의 Link를 style할 때는 styled(Link)
+
+export default withRouter(({ location: { pathname } }) => (
+  // * props.location.pathname에 Router가 가리키는 주소가 있다
+  <Header>
+    <List>
+      <Item current={pathname === '/'}>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item current={pathname === '/tv'}>
+        <SLink to="/tv">TV</SLink>
+      </Item>
+      <Item current={pathname === '/search'}>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
+));
 
 // * export default () => () : implicit return
 // * export default () => {} : doesn't, so return statement is needed
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,17 +3,24 @@ import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-d
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
+import Header from "./Header"
 
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" exact component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
 
-// ! <Router>는 only one chile element만 가질 수 있다
-// * <Route />는 <Router></Router> 안에 존재해야 한다
+// ! <Router>는 only one child element만 가질 수 있다
+// * <BrowserRouter />: HTML5 히스토리 API를 사용하여 주소를 관리하는 라우터(해쉬뱅 모드 사용 안함)
+// * <Route />: 요청 경로와 렌더링할 컴포넌트를 설정한다, <Router></Router> 안에 존재해야 한다
+// * <Switch />: 하위에 라우터 중 하나를 선택한다
+// * <Redirect />: 요청 경로를 다른 경로로 리다이렉션한다
