9e5c467d5c703c408d6dfdf594a454121ec2a57e
insight88 <64120339+insight88@users.noreply.github.com>
2020-11-03 19:42:39 +0900

    0.9 bug fix2

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import Home from "../Routes/Home";
-import TV from "../Routes/TV";
+import TV from "../Routes/TV"
 import Search from "../Routes/Search";
 import Detail from "../Routes/Detail";
 import Header from "./Header"
